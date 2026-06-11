6eeed0b48dbc2bfdb6dc914561d2fbf6120e75ea
bigguysahaj <sahajssingh2001@gmail.com>
2026-06-12 02:52:33 +0530

    Updated with current Progress

--- src/pages/movie.tsx
+++ src/pages/movie.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import { Inter } from 'next/font/google'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const inter = Inter({ subsets: ['latin'] })
 
+export const BASE_URL = 'https://api.letterboxd.com/api/v0';
+
 const MoviePage: React.FC = () => {
-  // Your movie page content here
+  const [loading, setLoading] = React.useState(true);
+  const [movies, setMovies] = useState([]);
+  
   return (
     <div
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
+
       <section className='py-24 flex flex-col items-center text-center
        gap-6'>
         <h1 className='text-4xl font-bold'>Movie Page</h1>
